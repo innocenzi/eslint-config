992c0f48c0e3540bb77e948a4fd7394fe11d065a
Enzo Innocenzi <enzo@innocenzi.dev>
2021-06-29 13:23:21 +0200

    chore(vue): disable no-unused

--- vue.js
+++ vue.js
@@ -4,6 +4,15 @@ module.exports = {
 		extraFileExtensions: ['.vue'],
 	},
 	extends: ['./typescript', 'plugin:vue/vue3-recommended'],
+	overrides: {
+		files: ['*.vue'],
+		rules: {
+			'no-unused-imports': 'off',
+			'no-unused-vars': 'off',
+			'unused-imports/no-unused-imports': 'off',
+			'unused-imports/no-unused-vars': 'off',
+		},
+	},
 	rules: {
 		'vue/html-indent': ['error', 'tab'],
 		'vue/html-self-closing': ['error', {
