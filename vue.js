d4d74d4fdba3f1cc4c3c7530872844b07ab12dd3
Enzo Innocenzi <enzo@innocenzi.dev>
2021-06-29 13:11:52 +0200

    chore: add self closing rule

--- vue.js
+++ vue.js
@@ -6,5 +6,14 @@ module.exports = {
 	extends: ['./typescript', 'plugin:vue/vue3-recommended'],
 	rules: {
 		'vue/html-indent': ['error', 'tab'],
+		'vue/html-self-closing': ['error', {
+			html: {
+				void: 'never',
+				normal: 'always',
+				component: 'always',
+			},
+			svg: 'always',
+			math: 'always',
+		}],
 	},
 }
