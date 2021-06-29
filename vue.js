df6c829fac2d3863caef7f4f54721597e983562b
Enzo Innocenzi <enzo@innocenzi.dev>
2021-06-29 13:24:19 +0200

    fix(vue): overrides format

--- vue.js
+++ vue.js
@@ -4,15 +4,17 @@ module.exports = {
 		extraFileExtensions: ['.vue'],
 	},
 	extends: ['./typescript', 'plugin:vue/vue3-recommended'],
-	overrides: {
-		files: ['*.vue'],
-		rules: {
-			'no-unused-imports': 'off',
-			'no-unused-vars': 'off',
-			'unused-imports/no-unused-imports': 'off',
-			'unused-imports/no-unused-vars': 'off',
+	overrides: [
+		{
+			files: ['*.vue'],
+			rules: {
+				'no-unused-imports': 'off',
+				'no-unused-vars': 'off',
+				'unused-imports/no-unused-imports': 'off',
+				'unused-imports/no-unused-vars': 'off',
+			},
 		},
-	},
+	],
 	rules: {
 		'vue/html-indent': ['error', 'tab'],
 		'vue/html-self-closing': ['error', {
