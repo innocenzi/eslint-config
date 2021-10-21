03a233a74dd8b1066c114709abd38b927c45e719
Enzo Innocenzi <enzo@innocenzi.dev>
2021-10-21 13:06:37 +0200

    chore: upgrade deps

--- preset.ts
+++ preset.ts
@@ -3,5 +3,5 @@ import { Preset } from 'apply'
 Preset.setName('eslint-config')
 Preset.extract()
 Preset.editNodePackages()
-	.addDev('eslint', '^7.29.0')
-	.addDev('@innocenzi/eslint-config', '^0.1.8')
+	.addDev('eslint', '^7.32.0')
+	.addDev('@innocenzi/eslint-config', 'latest')
