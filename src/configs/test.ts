227011c0184c1b5a925354c54dbea11c00692792
Enzo Innocenzi <enzo@innocenzi.dev>
2024-06-22 00:00:54 +0200

    fix(tests): prefer `test` as the test function

--- src/configs/test.ts
+++ src/configs/test.ts
@@ -45,7 +45,7 @@ export async function test(
 			rules: {
 				'node/prefer-global/process': 'off',
 
-				'test/consistent-test-it': ['error', { fn: 'it', withinDescribe: 'it' }],
+				'test/consistent-test-it': ['error', { fn: 'test', withinDescribe: 'it' }],
 				'test/no-identical-title': 'error',
 				'test/no-import-node-test': 'error',
 				'test/no-only-tests': isInEditor ? 'off' : 'error',
